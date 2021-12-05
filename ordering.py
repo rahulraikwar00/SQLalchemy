ce966e73e5c6982bb9c1e0aa129521f5834a88f2
rahul <rr200636@gmail.com>
2021-12-06 01:29:16 +0530

    add delete all user

--- ordering.py
+++ ordering.py
@@ -1,10 +1,12 @@
-from sqlalchemy.orm import session
 from main import User ,Session, engine
 
 session = Session(bind=engine)
 
 #assending order
-users = session.query(User).order_by(User.username).all()
+# users = session.query(User).order_by(User.username).all()
+
+#descending order
+users = session.query(User).order_by(User.username.desc()).all()
 
 for user in users:
     print(user.username, user.id, user.email)
